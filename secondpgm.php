2925b0826568c4fb7e381ef43febed6a2eea226e
Sivapriyadarsiniannamalai <sivapriyadarsiniannamalai@gmail.com>
2024-11-29 11:31:51 +0530

    Multidimensional arrays
    Multidimensional arrays in PHP

---
+++ secondpgm.php
@@ -0,0 +1,16 @@
+Multidimensional arrays
+<html>
+<body>
+<?php
+  $flowers = array(
+  array("Sunflower","Yellow",1967),
+  array("Rose","Red",1987),
+  array("Jasmine","White",2002),
+  array("Dahlia","Pink",2000)
+  );
+  echo $flowers[0][0]."<br>".$flowers[0][1]."<br>".$flowers[0][2].".br";
+  
+  ?>
+  
+  </body>
+  </html>
